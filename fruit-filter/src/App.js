3aad60bcf01954af95a95a39c5912f65fbcc5e6f
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2020-01-14 19:06:25 -0500

    add fruit list

---
+++ fruit-filter/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import data from './fruitListData'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fruit Filter</h1>
+    </div>
+  );
+}
+
+export default App;
